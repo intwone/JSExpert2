cc46676fb88682307973586515d363b6efbf0f6b
Cassio Oliveira Silva <cassiointw1993@gmail.com>
2021-03-24 16:22:51 -0300

    Added render table in console

--- live02/platforms/console/index.mjs
+++ live02/platforms/console/index.mjs
@@ -1,5 +1,35 @@
+import chalk from 'chalk'
+import chalkTable from 'chalk-table'
+
 export default class TableConsoleComponent {
   render(data) {
-    console.log('hi tableconsole!!!')
+    const colums = this.prepareData(data)
+
+    const options = {
+      leftPad: 2,
+      colums: colums
+    }
+
+    const table = chalkTable(options, data)
+
+    console.log(table);
+  }
+
+  prepareData(data) {
+    const [firstItem] = data
+    const header = Object.keys(firstItem)
+
+    const formatHeader = (data, index) => index % 2 === 0 
+      ? chalk.red(data)
+      : chalk.green(data);
+
+    
+
+    const colums = header.map((item, index) => ({
+      field: item,
+      name: formatHeader(item, index)
+    }))
+
+    return colums
   }
 }
